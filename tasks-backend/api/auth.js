346dab11f0540bbc2e1347560b37343576d811d6
Bruno Brandão Lopes <brandao_lopes@live.com>
2020-08-24 11:11:47 -0300

    autenticação de usuário - metodo signin

---
+++ tasks-backend/api/auth.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+const { authSecret } = require('../.env')
+const jwt = require('jwt-simple')
+const bcrypt = require('bcrypt-nodejs')
+const consign = require('consign/lib/consign')
+
+module.exports = app => {
+    const signin = async( req, res ) => {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).send('Dados incompletos')
+        }
+
+        const user = await app.db('users')
+            .where({ email: req.body.email })
+            .first()
+        if (user) {
+            bcrypt.compare(req.body.password, user.password, (erro, isMatch) => {
+                if (erro || !isMatch) {
+                    return res.status(401).send()
+                }
+
+                const payload = { id: user.id }
+                res.json({
+                    name: user.name, 
+                    email: user.email,
+                    token: jwt.encode(payload, authSecret),
+                })
+            })
+        } else {
+            res.status(400).send('Usuário não cadastrado')
+        }
+
+    }
+    return { signin }
+}
